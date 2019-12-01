a96ade9a351a21b1e4bcd3f4467d985b03ec0d57
YangYaochi <47493300+YangYaochi@users.noreply.github.com>
2019-12-01 23:15:12 +0800

    Add files via upload

---
+++ ssd1306/test.py
@@ -0,0 +1,15 @@
+#测试1：
+# import format_str
+# format_str.mk_cn_en_str("我爱你祖国22我爱你祖国22我爱你祖国22我爱你祖国22我爱你祖国22我爱你祖国22我爱你祖国22")
+#
+#
+#
+#测试2：
+# from ssd1306_lib import SSD1306_I2C
+# from machine import Pin, I2C
+# i2c = I2C(scl=Pin(5), sda=Pin(4))
+# oled = SSD1306_I2C(128, 64, i2c)
+# oled.draw_all(2, 127, 63, 0, 0)
+# oled.show()
+
+##测试1，2一次只能运行一个
